656b393600ccd6c9094e0ea4a067b0a3019449e6
Justin Huang <justinhuangthedragon@gmail.com>
2022-06-14 21:43:00 -0700

    IS657-HW3

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.colmcontainer}>
+      </View>
+      <View style={styles.rwcontainer}>
+        <View style={styles.rowFlexDesign}>
+          <View style={styles.rowBox}>
+            <Text style={styles.text}>IS657 Midterm</Text>
+          </View>
+        </View>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
 }
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
+
+  rowFlexDesign: {
+    flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  rowBox: {
+    borderWidth: 2,
+    backgroundColor: 'green',
+    borderColor: 'green',
+    height: 150,
+    width: 200,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 15,
+    marginRight: 15,
+  },
+  text: {
+    fontSize: 20,
+    marginBottom: 10,
+    fontWeight: 'bold',
+    marginHorizontal: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
+
   },
 });
